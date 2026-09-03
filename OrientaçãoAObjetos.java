2acb34bdddc49c50f504e17d4b66f98858d335df
FelipeFettback <wapffelipe@gmail.com>
2026-09-03 15:02:14 -0300

    Aulas de LocalDate, Classes, Relacionamento entre Classes e Métodos.
    Aulas sobre a API do JAVA LocalData, LocalDataTime, Instante e Duração, Programação Orientada a Objetos: Abstração, Classes, Relacionamento entre Classes e Métodos.
    Capítulo Não Completo ainda.

--- OrientaçãoAObjetos.java
+++ OrientaçãoAObjetos.java
@@ -69,3 +69,204 @@
 //     }
 // }
 
+// Realizando comparações entre datas.
+// LocalDate nascimento = LocalDate.of(1995, 5, 15);
+// LocalDate hoje = LocalDate.now();
+
+// Saber se é igual, antes ou depois.
+// System.out.println("Igual:" + nascimento.isEqual(hoje));
+// System.out.println("Antes:" + nascimento.isBefore(hoje));
+// System.out.println("Depois:" + nascimento.isAfter(hoje));
+
+// E o período decorrido?
+// Period periodo = Period.between(nascimento, hoje);
+
+// System.out.println("Período decorrido: " + periodo.getYears() + " anos, " 
+//     + periodo.getMonths() + " meses e " 
+//     + periodo.getDays() + " dias.");
+
+//Receber em meses
+// System.out.println("Período decorrido em meses: " + periodo.toTotalMonths());
+
+
+//--------------------------------------------------------------------------------//
+
+// Classes
+
+// public class Computador{
+
+//     String modelo;
+//     String marca;
+//     double preco;
+//     int memoriaRam;
+//     int espacamentoDisco;
+//     String processador;
+//     boolean placaDeVideoDedicada;
+// }
+
+// Abstração: é o processo de identificar os atributos e comportamentos essenciais de um objeto,
+// ignorando detalhes irrelevantes.
+
+// UML é uma linguagem de modelagem visual que permite representar graficamente os
+// elementos de um sistema orientado a objetos, como classes, atributos, métodos e 
+// relacionamentos entre eles. A UML é amplamente utilizada na engenharia de software 
+// para facilitar a comunicação entre desenvolvedores, analistas e outros stakeholders, 
+// além de auxiliar na documentação e no planejamento do desenvolvimento de sistemas complexos.
+
+// Instanciação: é o processo de criar um objeto a partir de uma classe, alocando memória para ele
+// e inicializando seus atributos. A instância é uma ocorrência específica de uma classe, com valores 
+// próprios para seus atributos.
+
+// Relacionamento entre classes: é a forma como as classes interagem entre si, podendo ser de
+// diferentes tipos, como associação, agregação, composição e herança.
+
+// Pasta org.example
+// ->App
+// ->Computador
+// ->Fabricante
+// ->Main
+
+
+// Dentro do App.java, vemos o relacionamento entre as classes Computador e Fabricante, 
+// onde a classe Computador possui um atributo do tipo Fabricante, 
+// representando a relação de associação entre elas.
+// ../App.java
+
+// package org.example;
+
+// public class App {
+//     public static void main(String[] args) {
+//         // Criando um objeto da classe Computador
+//         Computador pc = new Computador();
+//         pc.modelo = "Inspiron 15";
+//         pc.marca = "Dell";
+//         pc.preco = 3500.00;
+//         pc.memoriaRam = 8;
+//         pc.espacamentoDisco = 512;
+//         pc.placaDeVideoDedicada = false;
+//         pc.processador = "Intel Core;
+
+//         Fabricante dell = new Fabricante();
+//         dell.nome = "Dell Inc.";
+//         dell.endereco = "Estados Unidos";
+
+//         pc.fabricante = dell;
+
+
+// Dentro do Computador.java, podemos acessar os atributos da Classe Computador, 
+// que é o fabricante do computador.
+
+// ../Computador.java
+// package org.example;
+
+// public class Computador {]
+
+//     String modelo;
+
+//     String marca;
+
+//     double preco;
+
+//     int memoriaRam;
+
+//     int espacamentoDisco;
+
+//     String processador;
+
+//     boolean placaDeVideoDedicada;
+
+//     Fabricante fabricante; 
+
+//     }
+
+// Dentro do Fabricante.java, podemos acessar os atributos da Classe Fabricante.
+
+// ../Fabricante.java
+
+// package org.example;
+
+// public class Fabricante {
+
+//     String nome;
+
+//     String endereco;
+
+// }
+
+// As classes foram criadas separadamente, computador e fabricante, e dentro do App.java,
+// podemos instanciar objetos dessas classes e estabelecer o relacionamento entre elas.
+// No App.java, criamos um objeto da classe Computador, sendo ele o pc, e um objeto da classe Fabricante,
+// sendo ele o dell.
+
+
+// Métodos: são blocos de código que definem comportamentos ou ações que um objeto pode realizar.
+
+// ../App.java
+
+// package org.example;
+
+// public class App {
+//     public static void main(String[] args) {
+//         // Criando um objeto da classe Computador
+//         Computador pc = new Computador();
+//         pc.modelo = "Inspiron 15";
+//         pc.marca = "Dell";
+//         pc.preco = 3500.00;
+//         pc.memoriaRam = 8;
+//         pc.espacamentoDisco = 512;
+//         pc.placaDeVideoDedicada = false;
+//         pc.processador = "Intel Core;
+
+//         Fabricante dell = new Fabricante();
+//         dell.nome = "Dell Inc.";
+//         dell.endereco = "Estados Unidos";
+
+//         pc.fabricante = dell;
+
+//         double valorComDesconto = pc.calcularPrecoComDesconto(10); // Chamando o método calcularPrecoComDesconto com 10% de desconto (poderia utilizar 0.1 também)
+//         System.out.println("Preço com desconto: " + valorComDesconto);
+
+//         boolean sucesso = pc.adicionarMemoriaRam(4); // Chamando o método adicionarMemoriaRam com 4GB de memória adicional
+//         if (sucesso) {
+//           System.out.println("Memória RAM adicionada com sucesso. Nova quantidade: " + pc.memoriaRam + "GB");
+//           System.out.println("Novo preço: " + pc.preco);
+//         } else {
+//           System.out.println("Não foi possível adicionar memória RAM. Limite máximo atingido.");
+
+// Para isto funcionar, é necessário criar os métodos dentro da classe Computador.java, 
+// que são o calcularPrecoComDesconto e o adicionarMemoriaRam. E assim ficaria o código: 
+
+// ../Computador.java
+// package org.example;
+
+// public class Computador {
+
+//     String modelo;
+
+//     String marca;
+
+//     double preco;
+
+//     int memoriaRam;
+
+//     int espacamentoDisco;
+
+//     String processador;
+
+//     boolean placaDeVideoDedicada;
+
+//     Fabricante fabricante;
+
+//     public double calcularPrecoComDesconto(double percentualDesconto) {
+//         double desconto = preco * (percentualDesconto / 100);
+//         return preco - desconto;
+//     }
+
+        // boolean adicionarMemoriaRam(int quantidadeAdicional) {
+        //     if (quantidade % 2 == 1) return false; // Verifica se a quantidade adicional é ímpar
+        //     memoriaRam += quantidade;
+        //     preco += quantidade * 150; // Supondo que cada GB adicional custa R$150
+        //     return true;
+        // }
+
+//     }
